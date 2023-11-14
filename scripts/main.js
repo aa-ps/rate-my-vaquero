c940ffd6a5351bb934dfbc4377ce1e3630d3a4f9
Aaron <aaronpuli45@gmail.com>
2023-11-14 14:39:01 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,30 +2,40 @@ const SCHOOL_LEGACY_ID = 1306;
 const SCHOOL_ID = "U2Nob29sLTEzMDY=";
 const RMP_API_URL = "https://www.ratemyprofessors.com/graphql";
 const SEARCH_TABLE = document.getElementById("searchResultsTable");
+const QUERY =
+  "query NewSearchTeachersQuery(\n  $query: TeacherSearchQuery!\n  $count: Int\n) {\n  newSearch {\n    teachers(query: $query, first: $count) {\n      didFallback\n      edges {\n        cursor\n        node {\n          id\n          legacyId\n          firstName\n          lastName\n          department\n          departmentId\n          school {\n            legacyId\n            name\n            id\n          }\n          ...CompareProfessorsColumn_teacher\n        }\n      }\n    }\n  }\n}\n\nfragment CompareProfessorsColumn_teacher on Teacher {\n  id\n  legacyId\n  firstName\n  lastName\n  school {\n    legacyId\n    name\n    id\n  }\n  department\n  departmentId\n  avgRating\n  numRatings\n  wouldTakeAgainPercentRounded\n  mandatoryAttendance {\n    yes\n    no\n    neither\n    total\n  }\n  takenForCredit {\n    yes\n    no\n    neither\n    total\n  }\n  ...NoRatingsArea_teacher\n  ...RatingDistributionWrapper_teacher\n}\n\nfragment NoRatingsArea_teacher on Teacher {\n  lastName\n  ...RateTeacherLink_teacher\n}\n\nfragment RatingDistributionWrapper_teacher on Teacher {\n  ...NoRatingsArea_teacher\n  ratingsDistribution {\n    total\n    ...RatingDistributionChart_ratingsDistribution\n  }\n}\n\nfragment RatingDistributionChart_ratingsDistribution on ratingsDistribution {\n  r1\n  r2\n  r3\n  r4\n  r5\n}\n\nfragment RateTeacherLink_teacher on Teacher {\n  legacyId\n  numRatings\n  lockStatus\n}\n";
 
 class ProfessorData {
-  constructor(data) {
-    this.avgRating = data?.avgRating || 0;
-    this.department = data?.department || "";
-    this.firstName = data?.firstName || "";
-    this.id = data?.id || "";
-    this.lastName = data?.lastName || "";
-    this.legacyId = data?.legacyId || 0;
-    this.numRatings = data?.numRatings || 0;
-    this.wouldTakeAgainPercent = data?.wouldTakeAgainPercentRounded || 0;
+  constructor({
+    avgRating = 0,
+    department = "",
+    firstName = "",
+    id = "",
+    lastName = "",
+    legacyId = 0,
+    numRatings = 0,
+    wouldTakeAgainPercentRounded = 0,
+  } = {}) {
+    this.avgRating = avgRating;
+    this.department = department;
+    this.firstName = firstName;
+    this.id = id;
+    this.lastName = lastName;
+    this.legacyId = legacyId;
+    this.numRatings = numRatings;
+    this.wouldTakeAgainPercent = wouldTakeAgainPercentRounded;
   }
 }
 
 async function searchProfessor(name) {
   const queryBody = {
-    query:
-      "query NewSearchTeachersQuery(\n  $query: TeacherSearchQuery!\n  $count: Int\n) {\n  newSearch {\n    teachers(query: $query, first: $count) {\n      didFallback\n      edges {\n        cursor\n        node {\n          id\n          legacyId\n          firstName\n          lastName\n          department\n          departmentId\n          school {\n            legacyId\n            name\n            id\n          }\n          ...CompareProfessorsColumn_teacher\n        }\n      }\n    }\n  }\n}\n\nfragment CompareProfessorsColumn_teacher on Teacher {\n  id\n  legacyId\n  firstName\n  lastName\n  school {\n    legacyId\n    name\n    id\n  }\n  department\n  departmentId\n  avgRating\n  numRatings\n  wouldTakeAgainPercentRounded\n  mandatoryAttendance {\n    yes\n    no\n    neither\n    total\n  }\n  takenForCredit {\n    yes\n    no\n    neither\n    total\n  }\n  ...NoRatingsArea_teacher\n  ...RatingDistributionWrapper_teacher\n}\n\nfragment NoRatingsArea_teacher on Teacher {\n  lastName\n  ...RateTeacherLink_teacher\n}\n\nfragment RatingDistributionWrapper_teacher on Teacher {\n  ...NoRatingsArea_teacher\n  ratingsDistribution {\n    total\n    ...RatingDistributionChart_ratingsDistribution\n  }\n}\n\nfragment RatingDistributionChart_ratingsDistribution on ratingsDistribution {\n  r1\n  r2\n  r3\n  r4\n  r5\n}\n\nfragment RateTeacherLink_teacher on Teacher {\n  legacyId\n  numRatings\n  lockStatus\n}\n",
+    query: QUERY,
     variables: {
       query: {
         text: name,
         schoolID: SCHOOL_ID,
       },
-      count: 10,
+      count: 1,
     },
   };
 
@@ -41,22 +51,22 @@ async function searchProfessor(name) {
     }
   );
 
-  const data = await response.json();
-  if (data.data.newSearch.teachers.edges.length != 0) {
-    return new ProfessorData(data.data.newSearch.teachers.edges[0].node);
-  } else {
-    return new ProfessorData();
-  }
+  const { data } = await response.json();
+  return new ProfessorData(data?.newSearch.teachers.edges[0]?.node);
 }
 
 function getProfessors(searchTable) {
   const professorElements = Array.from(
     searchTable.querySelectorAll('td[data-property="instructor"]')
   );
-  const professorNames = professorElements.map(
-    (e) => e.querySelector("a").innerText
-  );
-  return { professorElements, professorNames };
+  return {
+    professorElements,
+    professorNames: professorElements.map((e) =>
+      e.querySelector("a") == null
+        ? "NO_PROFESSOR"
+        : e.querySelector("a").innerText
+    ),
+  };
 }
 
 function formatName(name) {
@@ -96,24 +106,26 @@ function insertRating(professor, professorElement) {
   });
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function displayRatings(searchTable) {
   const { professorElements, professorNames } = getProfessors(searchTable);
   for (let i = 0; i < professorNames.length; i++) {
-    let name = formatName(professorNames[i]);
-    const professor = await searchProfessor(name);
-    insertRating(professor, professorElements[i]);
+    const name = formatName(professorNames[i]);
+    if (name != "NO_PROFESSOR") {
+      const professor = await searchProfessor(name);
+      insertRating(professor, professorElements[i]);
+      await sleep(500);
+    }
   }
 }
 
-const observer = new MutationObserver((mutations) => {
-  displayRatings(SEARCH_TABLE);
-});
 
-chrome.storage.sync.get("enabled", function ({ enabled }) {
-  if (enabled) {
-    observer.observe(SEARCH_TABLE, {
-      subtree: true,
-      childList: true,
-    });
-  }
+const observer = new MutationObserver(() => displayRatings(SEARCH_TABLE));
+
+chrome.storage.sync.get("enabled", ({ enabled }) => {
+  if (enabled)
+    observer.observe(SEARCH_TABLE, { subtree: true, childList: true });
 });
